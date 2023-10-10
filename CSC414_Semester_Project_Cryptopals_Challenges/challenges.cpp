dfe87dea2870f28df118ae4399eb163f2c655f8f
Wyatt Dove <143294093+wyatt-dove@users.noreply.github.com>
2023-10-10 13:38:49 -0500

    Code for Challenge 3

--- CSC414_Semester_Project_Cryptopals_Challenges/challenges.cpp
+++ CSC414_Semester_Project_Cryptopals_Challenges/challenges.cpp
@@ -21,7 +21,48 @@ void challenge2()
 //logic for challenge 3: Single-byte XOR cipher (wyatt dove)
 void challenge3()
 {
+    //input hex string
+    string input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
 
+    //hex decode input
+    vector<unsigned char> inputBytes = hexDecode(input);
+
+    //variables to track current and high score for english plaintext, as well as the cipher key
+    double hiScore = 0;
+    double currScore = 0;
+    unsigned char key = ' ';
+
+    //vectors to store current and best english plaintext so far
+    string bestEnglish;
+    string currEnglish;
+
+    //test all possible character values (0-255) as the cipher key
+    for (unsigned int i = 0; i < 256; i++)
+    {
+        // char being used as the cipher
+        unsigned char cipher = i;
+
+        //xor the decoded input against current cipher to produce a plaintext candidate
+        currEnglish = xorChar(inputBytes, cipher);
+
+        //score the output based on english-ness
+        currScore = englishScore(currEnglish);
+
+        //track the english plaintext with the highest score alongside its cipher character
+        if (currScore > hiScore)
+        {
+            hiScore = currScore;
+            bestEnglish = currEnglish;
+            key = cipher;
+        }
+    }
+
+    //bestEnglish should now have the most english-like plaintext stored in it
+    //output results
+    cout << "\nChallenge 3: Single-byte XOR Cipher" << endl;      //header
+    cout << "\nHex Input: " << input << endl;                   //hex input string
+    cout << "Decrypted Output: " << bestEnglish << endl;        //decrypted english plaintext string
+    cout << "Cipher Key: " << key << endl;                      //character used for encryption
 }
 
 //logic for challenge 4: Detect single-character XOR (kristopher gavin)
