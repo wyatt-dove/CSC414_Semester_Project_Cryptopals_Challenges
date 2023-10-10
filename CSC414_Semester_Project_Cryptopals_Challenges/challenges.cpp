ebed9b1af3d44017795f849ea6b1ae632425e3a5
jl42geek <144966048+jl42geek@users.noreply.github.com>
2023-10-10 18:46:57 -0500

    Update challenges.cpp
    Added Challenge 1 code that converts a hex string into base64.

--- CSC414_Semester_Project_Cryptopals_Challenges/challenges.cpp
+++ CSC414_Semester_Project_Cryptopals_Challenges/challenges.cpp
@@ -9,7 +9,74 @@ unsigned char CH7_KEY[16] = { 0x59, 0x45, 0x4C, 0x4C, 0x4F, 0x57, 0x20, 0x53, 0x
 //logic for challenge 1: Hex to Base64 (jordan livingston)
 void challenge1()
 {
+const char base64_chars[] =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"abcdefghijklmnopqrstuvwxyz"
+"0123456789+/";
 
+std::string base64_encode(const std::vector<unsigned char>& bytes_to_encode) {
+    int in_len = bytes_to_encode.size();
+    int i = 0;
+    int j = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
+    std::string ret;
+
+    while (in_len--) {
+        char_array_3[i++] = bytes_to_encode[j++];
+        if (i == 3) {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
+
+            for (i = 0; i < 4; i++)
+                ret += base64_chars[char_array_4[i]];
+            i = 0;
+            }
+        }
+
+    if (i) {
+        for (j = i; j < 3; j++)
+            char_array_3[j] = '\0';
+
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+
+        for (j = 0; j < i + 1; j++)
+            ret += base64_chars[char_array_4[j]];
+
+        while (i++ < 3)
+            ret += '=';
+        }
+
+    return ret;
+    }
+
+std::vector<unsigned char> hex_to_bytes(const std::string& hex) {
+    std::vector<unsigned char> bytes;
+
+    for (unsigned int i = 0; i < hex.length(); i += 2) {
+        std::string byteString = hex.substr(i, 2);
+        unsigned char byte = (unsigned char)strtol(byteString.c_str(), NULL, 16);
+        bytes.push_back(byte);
+        }
+
+    return bytes;
+    }
+
+void convert_hex_to_base64() {
+    std::string hex_str;
+    std::cout << "Enter a hex string: ";
+    std::cin >> hex_str;
+
+    auto bytes = hex_to_bytes(hex_str);
+    auto base64_str = base64_encode(bytes);
+
+    std::cout << "\nThe base64 representation is: " << base64_str << '\n';
+    
+    }
 }
 
 //logic for challenge 2: Fixed XOR (cole brady)
